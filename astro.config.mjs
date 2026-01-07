258e0369cdd2b6ee14a7228275ce36f7a1a43011
Fabio <fabio.riva@oto.agency>
2026-01-07 16:58:49 +0100

    centralizzazioni file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +19,11 @@ export default defineConfig({
   vite: {
     ssr: {
       external: ['firebase-admin']
+    },
+    resolve: {
+      alias: {
+        '@shared': path.resolve(__dirname, './shared'),
+      },
     }
   }
 });
--- src/scripts/schemas/entityFactory.js
+++ src/scripts/schemas/entityFactory.js
@@ -1,2 +1,2 @@
 // Re-export dalla fonte condivisa in /shared/schemas
-export * from '../../../shared/schemas/entityFactory.js';
+export * from '@shared/schemas/entityFactory.js';
